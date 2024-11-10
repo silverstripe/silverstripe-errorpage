145355a359f6079c7cd645d77cae9d9421445a5a
Guy Sartorelli <guy.sartorelli@gmail.com>
2024-11-11 11:54:50 +1300

    API Update code to reflect changes in silverstripe/cms

--- src/ErrorPage.php
+++ src/ErrorPage.php
@@ -60,11 +60,6 @@ class ErrorPage extends Page
 
     private static $allowed_children = [];
 
-    /**
-     * @deprecated 5.4.0 use class_description instead.
-     */
-    private static $description = 'Custom content for different error cases (e.g. "Page not found")';
-
     private static $class_description = 'Custom content for different error cases (e.g. "Page not found")';
 
     private static $icon_class = 'font-icon-p-error';
